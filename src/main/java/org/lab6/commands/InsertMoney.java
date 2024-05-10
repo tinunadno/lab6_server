0679a2446e05da800e976e3a15874bcd630ae387
tinunadno <perminov1901@bk.ru>
2024-05-10 14:23:47 +0300

    added passwords hashing

--- src/main/java/org/lab6/commands/InsertMoney.java
+++ src/main/java/org/lab6/commands/InsertMoney.java
@@ -6,33 +6,43 @@ import org.lab6.mainClasses.ResponseManager;
 
 import java.sql.SQLException;
 
-public class InsertMoney extends Command implements CommandWithArgument, UserIdRequire{
+public class InsertMoney extends Command implements CommandWithArgument, UserIdRequire {
     private String argument;
     private int userID;
     private String userName;
-    public InsertMoney(){
+
+    public InsertMoney() {
         this.setRequiresArgument(true);
         this.setRequiresUserID(true);
     }
+
     @Override
-    public void execute(){
-        int sum=Integer.parseInt(argument);
+    public void execute() {
+
         try {
+            int sum = Integer.parseInt(argument);
             LabWorkDAO.insertMoney(userID, sum);
-            responseManager.append("successfully added "+sum+" to your wallet");
-        }catch (SQLException e){
+            responseManager.append("successfully added " + sum + " to your wallet");
+        } catch (SQLException e) {
             responseManager.append("can't insert money on your account, check data you inserted");
+        } catch (NumberFormatException e) {
+            responseManager.append("cannot convert \"" + argument + "\" to LabWork ID.");
         }
     }
+
     @Override
-    public void setUserId(int userID, String userName){
-        this.userID=userID;
-        this.userName=userName;
+    public void setUserId(int userID, String userName) {
+        this.userID = userID;
+        this.userName = userName;
     }
+
     @Override
-    public String getComment(){
+    public String getComment() {
         return "insert_money%внести денег на счет";
     }
+
     @Override
-    public void setArgument(String arg){this.argument=arg;}
+    public void setArgument(String arg) {
+        this.argument = arg;
+    }
 }
